b90ba741eb74f2fd208ad75c9d1e40699a5a1703
Ludderzz <91859792+Ludderzz@users.noreply.github.com>
2026-07-27 12:58:51 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,6 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
-import { appRouter } from "../server/routers"; // Removed .ts extension
-import { createContext } from "../server/_core/context";
+import { appRouter } from "../server/routers.js";          // Change from .ts to .js
+import { createContext } from "../server/_core/context.js"; // Add .js
 import express from "express";
 import cors from "cors";
 
